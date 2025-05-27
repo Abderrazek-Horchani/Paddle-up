8e46592b209b4edbf175c79230f20f9482eeac14
Rzouga <rzouga1712@gmail.com>
2025-05-27 14:59:38 +0100

    Fix: Enable mobile navbar toggler by adding Bootstrap JS bundle

--- paddleup-nextjs/pages/_app.tsx
+++ paddleup-nextjs/pages/_app.tsx
@@ -1,8 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/style.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'; // Assuming you will install Font Awesome via npm
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap.bundle.min.js');
+  }, []);
   return <Component {...pageProps} />;
 }
 
